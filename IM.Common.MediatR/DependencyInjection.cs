48288aa4e227662520dfd2b0a18b3f5eacd61091
ivan-man <santa-kljauznik@mail.ru>
2022-08-08 21:49:00 +0300

    Mediatr

---
+++ IM.Common.MediatR/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using IM.Common.MediatR.Behaviours;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IM.Common.MediatR;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddImMediatr(this IServiceCollection services, Type lookupType)
+    {
+        services.AddMediatR(lookupType ?? throw new ArgumentNullException(nameof(lookupType)));
+
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(FluentValidationBehaviour<,>));
+
+        return services;
+    }
+}
